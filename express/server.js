e7ab729f5c49a7c041cf774892df2cdcd6059eb3
JiHun CHON 천지훈 <jh.chon@dzsi.com>
2021-01-15 13:35:21 +0900

    added Router

--- express/server.js
+++ express/server.js
@@ -11,12 +11,13 @@ const util = require('../src/utils');
 const fs = require('fs');
 
 
-//const router = express.Router();
 //router.get('/', (req, res) => {
 //  res.writeHead(200, { 'Content-Type': 'text/html' });
 //  res.write('<h1>Hello from Express.js!</h1>');
 //  res.end();
 //});
+
+const router = express.Router();
 
 // Other settings
 app.set('view engine', 'ejs');
